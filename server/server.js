b2ee2712f1c1a269af86f1693b88b77a4887511f
Raul <r.vargas@tesipro.com>
2018-10-23 12:06:36 +0200

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const bodyParser = require('body-parser')
 
@@ -10,36 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-});
+app.use(require('./routes/usuario.js'));
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
+mongoose.connect(process.env.URLDB, (err, res) => {
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
+    if (err) throw err;
 
-        res.json({
-            persona: body
-        });
-    }
-});
+    console.log('Base de datos ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
 });
 
 app.listen(process.env.PORT, () => {
