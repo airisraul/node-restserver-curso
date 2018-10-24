61c602cee62a3624c8fb2390bcfcff227577c189
Raul <r.vargas@tesipro.com>
2018-10-24 10:34:53 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123456@ds133054.mlab.com:33054/cafe';
+    urlDB = process.env.MONGO_URI;
+    //'mongodb://cafe-user:abc123456@ds133054.mlab.com:33054/cafe';
 }
 process.env.URLDB = urlDB;
