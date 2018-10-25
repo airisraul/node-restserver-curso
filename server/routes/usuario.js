92b8c2b1af7c89ef6bdbd4491817e817f050dc68
Raul <r.vargas@tesipro.com>
2018-10-25 08:43:48 +0200

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const Usuario = require('../models/usuario');
 
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
+const Usuario = require('../models/usuario');
+
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 const app = express()
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, function(req, res) {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -36,12 +38,9 @@ app.get('/usuario', function(req, res) {
 
         });
 
-
-
-
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
 
@@ -70,7 +69,7 @@ app.post('/usuario', function(req, res) {
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -89,7 +88,7 @@ app.put('/usuario/:id', function(req, res) {
     });
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
 
@@ -116,14 +115,9 @@ app.delete('/usuario/:id', function(req, res) {
         res.json({
             ok: true,
             usuario: usuarioBorrado
-        })
+        });
 
-    })
-
-
-
-
-
+    });
 
 });
 
